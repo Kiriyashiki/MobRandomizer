e1aa26ab304b9f8548c10ec03f2a41424e430e49
MeijiRestored <42336759+MeijiRestored@users.noreply.github.com>
2025-06-05 19:18:38 +0200

    dep update, code improvements
    (cherry picked from commit 71094b6c66a34d33d47a3c05935df1920d509562)

--- src/main/java/be/meiji/omakasemobu/MobRandomizerMod.java
+++ src/main/java/be/meiji/omakasemobu/MobRandomizerMod.java
@@ -5,7 +5,6 @@ import static be.meiji.omakasemobu.util.TomlHelper.writeStringList;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -89,16 +88,16 @@ public class MobRandomizerMod implements ModInitializer {
       return null;
     }
 
+    newEntity.copyPositionAndRotation(entity);
+
     if (doInit && entity instanceof MobEntity) {
       ((MobEntity) newEntity).initialize(world, world.getLocalDifficulty(newEntity.getBlockPos()),
           SpawnReason.TRIGGERED, null);
     }
 
-    newEntity.copyPositionAndRotation(entity);
-
-    if (entity instanceof MobEntity && newEntity instanceof MobEntity
-        && ((MobEntity) entity).isPersistent()) {
-      ((MobEntity) newEntity).setPersistent();
+    if (entity instanceof MobEntity mobEntity && newEntity instanceof MobEntity newMobEntity
+        && mobEntity.isPersistent()) {
+      newMobEntity.setPersistent();
     }
 
     if (entity.hasVehicle()) {
@@ -206,8 +205,8 @@ public class MobRandomizerMod implements ModInitializer {
                 && MobRandomizerMod.canRandomize(entity.getType())) {
               Entity newEntity = createRandomizedEntity(world, entity, true);
 
-              if (newEntity instanceof MobEntity) {
-                ((MobEntity) newEntity).setPersistent();
+              if (newEntity instanceof MobEntity newMobEntity) {
+                newMobEntity.setPersistent();
               }
 
               if (newEntity != null) {
